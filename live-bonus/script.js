29fc182d2f3e29407922a47d6e1272804864255a
Sarah <richefeusarah@gmail.com>
2023-07-29 09:59:55 +0200

    Live bonus 1

--- live-bonus/script.js
+++ live-bonus/script.js
@@ -1,2 +1,11 @@
 console.log('V1 : Mon dico anglais')
 
+/*
+MON PROGRAMME : 
+> Je veux pouvoir donner la définition d'un mot à mes utilisateurs
+- Récupérer le mot saisi par l'utilisateur
+- Envoyer le mot à l'API ( https://dictionaryapi.dev/ )
+- Récupérer la définition du mot
+- Afficher la définition du mot
+- Ajouter un lecteur pour écouter la prononciation du mot
+*/
