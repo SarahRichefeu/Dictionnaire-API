8852735998f881280d35423019e791cf65d0d583
Sarah <richefeusarah@gmail.com>
2023-07-29 15:40:45 +0200

    Fin Live 2

--- live-bonus/script.js
+++ live-bonus/script.js
@@ -41,11 +41,65 @@ const apiCall = (word) => {
         .then((response) => response.json() )
         .then((data) => {
             // on récup le premier élément du tableau qui contient les infos du mot
-            const wordData = data[0]
-            // le tableau data contient des objets
-            console.log(wordData.word)
+            const infos = extractData(data[0])
+            renderToHtml(infos)
+        })
+        .catch( () => {
+            console.log('Le mot demandé n\'existe pas')
         })
 }
+
+// Fonction qui récupère chaque donnée que je veux et les renvoie dans un objet infos plus haut
+const extractData = (data) => {
+    // Le mot
+    const word = data.word 
+    // La phonétique (on va chercher le premier élément text)
+    const phonetic = findProp(data.phonetics, "text")
+    // const phonetic = data.phonetics[1].text
+    // L'audio
+    const audio = findProp(data.phonetics, "audio")
+    // Les définitions (on récup tout)
+    const meanings = data.meanings
+
+    return {
+        word: word,
+        phonetic: phonetic,
+        audio: audio,
+        meanings: meanings
+    }
+
+    // const definition1 = data.meanings[0].definitions[0].definition
+    // const partOfSpeech1 = data.meanings[0].partOfSpeech
+    // const definition2 = data.meanings[1].definitions[0].definition
+    // const partOfSpeech2 = data.meanings[1].partOfSpeech
+}
+
+
+// elle attend en argument un tableau à parcourir et une prop à chercher
+const findProp = (array, prop) => {
+    //Elle parcourt un tableau d'objets
+    for (let i = 0; i < array.length; i++) {
+        // Et cherche si l'objet contient une certaine propriété
+        if (array[i].hasOwnProperty(prop)) {
+            // ALors elle renvoie cette propriété (le return arrête la boucle)
+            return array[i][prop]
+        }
+    }
+}
+
+const renderToHtml = (data) => {
+    console.log(data)
+}
+
+
+/* il faudra afficher du coup : 
+    - .card_title pour le mot 
+    - .card_phonetic pour la phonétique
+    - .card_bloc
+    - .card_part-of-speech pour le type
+    - .card_definition pour la def 
+*/
+
 
 // Lancement du programme
 watchSubmit()
